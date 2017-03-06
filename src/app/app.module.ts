14a66fcf4d7c2513517e99e01e6add5ac198bfe2
Pongpon Tienthong <pongpon_tie@ascendinter.com>
2017-03-06 22:18:14 +0700

    Test Full Calender

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+// TODO: remove this component
+import { FullCalenderComponent } from './full-calender/full-calender.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FullCalenderComponent // TODO: remove this component
   ],
   imports: [
     BrowserModule,
