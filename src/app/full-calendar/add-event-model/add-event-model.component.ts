ab69ccb59b472fda1d8c4fc3422f7dda081d7436
Pongpon Tienthong <pongpon_tie@ascendinter.com>
2017-03-16 00:18:05 +0700

    - Add datetimepicker - Change form structure

--- src/app/full-calendar/add-event-model/add-event-model.component.ts
+++ src/app/full-calendar/add-event-model/add-event-model.component.ts
@@ -1,20 +1,29 @@
-import { Component, HostListener } from '@angular/core';
+import {Component, AfterViewInit} from '@angular/core';
 import { ModalComponent, DialogRef, CloseGuard } from "angular2-modal";
-
 import { AddEventModelContext } from "./add-event-model-context";
+declare var $:any;
 
 @Component({
   selector: 'app-add-event-model',
   templateUrl: './add-event-model.component.html',
   styleUrls: ['./add-event-model.component.css']
 })
-export class AddEventModelComponent implements CloseGuard, ModalComponent<AddEventModelContext> {
+export class AddEventModelComponent implements AfterViewInit, CloseGuard, ModalComponent<AddEventModelContext> {
 
   public openDialog: boolean;
 
   constructor(public dialog: DialogRef<AddEventModelContext>) {
     this.openDialog = true;
     dialog.setCloseGuard(this);
+  }
+
+  ngAfterViewInit() {
+    $(function() {
+      $('.datetimepicker').datetimepicker({
+        format: 'D/MM/YYYY HH:mm',
+        stepping: 15
+      });
+    });
   }
 
   // TODO: close modal when press ESC
@@ -39,8 +48,6 @@ export class AddEventModelComponent implements CloseGuard, ModalComponent<AddEve
     this.openDialog = false;
     this.dialog.close();
   }
-
-
 
 }
 
