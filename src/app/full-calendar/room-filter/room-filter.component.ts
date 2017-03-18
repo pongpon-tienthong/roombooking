f60c75f8e59c7a6307464a6dc72a7a75082330e3
Pongpon Tienthong <pongpon_tie@ascendinter.com>
2017-03-18 14:49:16 +0700

    add loading effect

--- src/app/full-calendar/room-filter/room-filter.component.ts
+++ src/app/full-calendar/room-filter/room-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, transition, style, animate } from '@angular/core';
 import { RoomService } from "../shared/room.service";
 import { Room } from "../shared/room";
 
@@ -6,14 +6,20 @@ import { Room } from "../shared/room";
   selector: 'room-filter',
   templateUrl: './room-filter.component.html',
   styleUrls: ['./room-filter.component.css'],
-  providers: [RoomService]
+  providers: [RoomService],
+  animations: [
+    trigger('visibilityChanged', [
+      state('shown' , style({ opacity: 1 })),
+      state('hidden', style({ opacity: 0 })),
+      transition('* => *', animate('0.5s'))
+    ])
+  ]
 })
 export class RoomFilterComponent implements OnInit {
 
   rooms: Room[] = [];
   allRoom: Room = new Room(0, 'All Rooms', 'bg-red');
   isLoading: boolean = true;
-
 
   constructor(private roomService: RoomService) { }
 
